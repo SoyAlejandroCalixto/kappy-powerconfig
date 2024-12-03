bd12c98598caa1eaa15266c4ffc403d80b4f0c6f
= <soyalejandrocalixto@gmail.com>
2024-12-03 09:38:24 +0000

    wezterm config changed

--- system/terminal.ps1
+++ system/terminal.ps1
@@ -36,6 +36,7 @@ local wezterm = require("wezterm")
 return {
     font = wezterm.font_with_fallback({ "CaskaydiaCove Nerd Font" }),
     font_size = 12.0,
+    freetype_load_target = "Light",
     color_scheme = "OneDark (base16)",
     hide_tab_bar_if_only_one_tab = true,
     window_close_confirmation = "NeverPrompt",
