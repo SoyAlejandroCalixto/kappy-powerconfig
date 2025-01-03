f76fed498e71d434a5446f5981a4bc4b5f8594b3
SoyAlejandroCalixto <soyalejandrocalixto@gmail.com>
2025-01-03 12:30:20 +0000

    terminal update

--- system/terminal.ps1
+++ system/terminal.ps1
@@ -18,21 +18,10 @@ WriteFile -path $PROFILE -content @"
 
 clear
 oh-my-posh init pwsh --config "C:\Users\kappy\AppData\Local\Programs\oh-my-posh\themes\thecyberden.omp.json" | Invoke-Expression
-fastfetch --logo $PROFILE\..\on_terminal_enter.txt --logo-color-1 blue --logo-color-2 blue --logo-padding-right 2 --color green -s os:kernel:uptime:shell:cpuUsage:memory --key-type icon --separator "  "
 Import-Module Terminal-Icons
 fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
 Set-Alias vim nvim
 
-"@
-
-# fastfetch ascii
-WriteFile -path $PROFILE\..\on_terminal_enter.txt -content @"
-   ___
-___|$|___
-|Z@%#%@Z|
-   |$|
-   |$|
-   |Z|
 "@
 
 # powershell modules
@@ -43,30 +32,62 @@ sudo Install-Module -Name PSReadLine -AllowClobber -Force
 WriteFile -path "$env:USERPROFILE\.wezterm.lua" -content @"
 
 local wezterm = require("wezterm")
+
+wezterm.on('update-right-status', function(window, pane)
+
+    local cwd = tostring(pane:get_current_working_dir())
+    cwd = cwd:gsub("file:///C:/", "")
+    cwd = cwd:gsub("Users/"..os.getenv("USERNAME").."/", "~")
+    cwd = " "..cwd.." 󰝰 "
+
+    local hostname = wezterm.hostname()
+    hostname = " "..hostname.."  "
+
+    local elements = {
+        { Foreground = { Color = "#c084fc" } },
+        { Text = utf8.char(0xe0b2) },
+        { Foreground = { Color = "#000000" } },
+        { Background = { Color = "#c084fc" } },
+        { Text = cwd },
+        { Foreground = { Color = "#60a5fa" } },
+        { Text = utf8.char(0xe0b2) },
+        { Foreground = { Color = "#000000" } },
+        { Background = { Color = "#60a5fa" } },
+        { Text = hostname },
+    }
+
+    window:set_right_status(wezterm.format(elements))
+
+end)
+
 return {
-    window_background_opacity = 0.8,
+    window_background_opacity = 0.9,
     win32_system_backdrop = "Acrylic",
     font = wezterm.font_with_fallback({ "CaskaydiaCove Nerd Font" }),
-    font_size = 13.0,
+    font_size = 14.0,
     freetype_load_target = "Light",
-    color_scheme = "OneDark (base16)",
-    hide_tab_bar_if_only_one_tab = true,
+    color_scheme = "Dracula (Official)",
     window_close_confirmation = "NeverPrompt",
     default_prog = { 'pwsh', '-NoLogo' },
     initial_rows = 32,
     initial_cols = 90,
     use_fancy_tab_bar = false,
-
+    window_padding = {
+        right = 0,
+        left = 0,
+        top = 0,
+        bottom = 0,
+    },
     colors = {
         tab_bar = {
-            background = "#282c34",
-            active_tab = { bg_color = "#c679dc", fg_color = "#000000" },
-            inactive_tab = { bg_color = "#301d36", fg_color = "#abb3be" },
-            inactive_tab_hover = { bg_color = "#301d36", fg_color = "#abb3be"},
-            new_tab = { bg_color = "#301d36", fg_color = "#808080" },
-            new_tab_hover = { bg_color = "#301d36", fg_color = "#808080" },
+            background = "#21222c",
+            active_tab = { bg_color = "#c084fc", fg_color = "#000000" },
+            inactive_tab = { bg_color = "#2e1065", fg_color = "#6273a5" },
+            inactive_tab_hover = { bg_color = "#2e1065", fg_color = "#6273a5"},
+            new_tab = { bg_color = "#2e1065", fg_color = "#6273a5" },
+            new_tab_hover = { bg_color = "#2e1065", fg_color = "#6273a5" },
         },
-    }
+    },
 }
 
 "@
@@ -84,7 +105,7 @@ WriteFile -path "$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\themes\thecy
       "alignment": "left",
       "segments": [
         {
-          "background": "#61afef",
+          "background": "#ba91f5",
           "foreground": "#282c34",
           "leading_diamond": "\ue0b6",
           "powerline_symbol": "\ue0b0",
@@ -93,7 +114,7 @@ WriteFile -path "$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\themes\thecy
           "type": "session"
         },
         {
-          "background": "#c8ccd4",
+          "background": "#f8f8f2",
           "foreground": "#282c34",
           "powerline_symbol": "\ue0b0",
           "properties": {
